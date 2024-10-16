e67cdd85ba642c0f71116c4d941a94150e7ce6e7
jeus1998 <zeus20240228@gmail.com>
2024-10-16 21:26:39 +0900

    [2024:10:16] 디자인 패턴 공부용 프젝 생성

---
+++ src/main/java/com/example/desginpatternstudy/DesginPatternStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.desginpatternstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesginPatternStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesginPatternStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desginpatternstudy/DesginPatternStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desginpatternstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesginPatternStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
