aa1a76a0353831b8d398d48e33f1a5263c6bd82d
jeus1998 <zeus20240228@gmail.com>
2024-10-17 18:31:29 +0900

    [2024:10:17] 싱글톤 패턴3: 싱글톤 패턴을 깨트리는 방법

--- src/main/java/com/example/desginpatternstudy/App.java
+++ src/main/java/com/example/desginpatternstudy/App.java
@@ -1,8 +1,29 @@
 package com.example.desginpatternstudy;
 
+import java.io.*;
+import java.lang.reflect.Constructor;
+
 public class App {
-    public static void main(String[] args) {
-        Settings settings = Settings.getInstance();
-        System.out.println(settings == Settings.getInstance()); // -> true
+    public static void main(String[] args) throws Exception{
+        Settings settings1 = Settings.getInstance();
+
+        // 직렬화 (try-with-resources)
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
+             out.writeObject(settings1);
+        }
+        Settings settings2 = null;
+        // 역직렬화
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
+             settings2 = (Settings) in.readObject();
+        }
+        System.out.println(settings1 != settings2); // true
     }
 }
+/*
+리플렉션 활용하기
+Settings settings1 = Settings.getInstance();
+Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+constructor.setAccessible(true);
+Settings settings2 = constructor.newInstance();
+System.out.println(settings1 != settings2); // -> true
+ */
