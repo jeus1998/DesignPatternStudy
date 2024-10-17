856136aad348e4771cb86d5524c088edb4d9aa78
jeus1998 <zeus20240228@gmail.com>
2024-10-17 20:58:49 +0900

    [2024:10:17] 싱글톤 패턴4: enum 활용 싱글톤

--- src/main/java/com/example/desginpatternstudy/App.java
+++ src/main/java/com/example/desginpatternstudy/App.java
@@ -2,28 +2,32 @@ package com.example.desginpatternstudy;
 
 import java.io.*;
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception{
-        Settings settings1 = Settings.getInstance();
-
-        // 직렬화 (try-with-resources)
-        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
-             out.writeObject(settings1);
-        }
+        Settings settings1 = Settings.INSTANCE;
         Settings settings2 = null;
-        // 역직렬화
-        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
-             settings2 = (Settings) in.readObject();
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
+            out.writeObject(settings1);
         }
-        System.out.println(settings1 != settings2); // true
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
+            settings2 = (Settings) in.readObject();
+        }
+        System.out.println(settings1 == settings2); // -> true
     }
 }
 /*
-리플렉션 활용하기
-Settings settings1 = Settings.getInstance();
-Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
-constructor.setAccessible(true);
-Settings settings2 = constructor.newInstance();
-System.out.println(settings1 != settings2); // -> true
+리플렉션에 안전 -> IllegalArgumentException: Cannot reflectively create enum objects
+public class App {
+    public static void main(String[] args) throws Exception{
+        Settings settings1 = Settings.INSTANCE;
+        Settings settings2 = null;
+        Constructor<?>[] constructors = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            constructor.setAccessible(true);
+            settings2 = (Settings) constructor.newInstance("INSTANCE");
+        }
+    }
+}
  */
