6faf4063750c2ec7d32ca3c53808e18c7163ac0b
jeus1998 <zeus20240228@gmail.com>
2024-10-19 13:22:08 +0900

    [2024:10:19] 빌더 패턴1: 패턴 적용 before

---
+++ src/main/java/com/example/desginpatternstudy/_01_creational_patterns/_04_builder/_01_before/TourPlan.java
@@ -0,0 +1,21 @@
+package com.example.desginpatternstudy._01_creational_patterns._04_builder._01_before;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter @ToString
+@AllArgsConstructor @NoArgsConstructor
+public class TourPlan {
+    private String title;
+    private int nights;
+    private int days;
+    private LocalDate startDate;
+    private String whereToStay;
+    private List<DetailPlan> plans = new ArrayList<>();
+    public void addPlan(int day, String plan){
+        plans.add(new DetailPlan(day, plan));
+    }
+}
