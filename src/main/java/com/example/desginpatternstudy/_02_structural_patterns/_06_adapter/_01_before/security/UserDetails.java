55ddece41064a77a7b92d38b5c6dc4fc1c250f60
jeus1998 <zeus20240228@gmail.com>
2024-10-22 18:44:12 +0900

    [2024:10:22] 어댑터 패턴1: 패턴 before

---
+++ src/main/java/com/example/desginpatternstudy/_02_structural_patterns/_06_adapter/_01_before/security/UserDetails.java
@@ -0,0 +1,5 @@
+package com.example.desginpatternstudy._02_structural_patterns._06_adapter._01_before.security;
+public interface UserDetails {
+    String getUsername();
+    String getPassword();
+}
