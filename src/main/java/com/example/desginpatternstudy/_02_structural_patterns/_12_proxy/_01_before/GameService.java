1cc97805980fa6f3bc3d82bee3f190838e05eb0a
jeus1998 <zeus20240228@gmail.com>
2024-10-30 20:36:14 +0900

    [2024:10:30] 프록시 패턴: before

---
+++ src/main/java/com/example/desginpatternstudy/_02_structural_patterns/_12_proxy/_01_before/GameService.java
@@ -0,0 +1,10 @@
+package com.example.desginpatternstudy._02_structural_patterns._12_proxy._01_before;
+
+public class GameService {
+    public void gameService1(){
+        System.out.println("gameService1");
+    }
+    public void gameService2(){
+        System.out.println("gameService2");
+    }
+}
