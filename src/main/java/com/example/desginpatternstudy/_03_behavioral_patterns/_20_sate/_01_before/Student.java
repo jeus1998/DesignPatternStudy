d14f689c3e0ef6177547e85643e52184d94a2f98
jeus1998 <zeus20240228@gmail.com>
2024-11-25 16:45:48 +0900

    [2024:11:25] 상태 패턴: before

---
+++ src/main/java/com/example/desginpatternstudy/_03_behavioral_patterns/_20_sate/_01_before/Student.java
@@ -0,0 +1,16 @@
+package com.example.desginpatternstudy._03_behavioral_patterns._20_sate._01_before;
+import java.util.*;
+
+public class Student {
+    private String name;
+    public Student(String name){
+        this.name = name;
+    }
+    private List<OnlineCourse> privateCourses = new ArrayList<>();
+    public boolean isEnabledForPrivateClass(OnlineCourse onlineCourse){
+        return this.privateCourses.contains(onlineCourse);
+    }
+    public void addCourse(OnlineCourse onlineCourse){
+        this.privateCourses.add(onlineCourse);
+    }
+}
